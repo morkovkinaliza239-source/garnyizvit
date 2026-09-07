814b3c45f0d71934db56c4ee154769d681248dcd
morkovkinaliza239-source <morkovkinaliza239@gmail.com>
2026-09-07 14:20:26 +0300

    Update astro.config.mjs

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -1,8 +1,5 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
-
 export default defineConfig({
   site: 'https://garnyizvit.com.ua',
-  integrations: [sitemap()],
   trailingSlash: 'always',
 });
